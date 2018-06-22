7eb2f4d35e6cf00abbb904644a3c91e834f47ac3
EricJPogue <epogue@epogue.com>
2018-06-22 10:49:21 -0500

    Update files.

--- save-contacts.php
+++ save-contacts.php
@@ -1,14 +1,7 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
     $contacts = $_POST["contacts"];
     $myfile = fopen("contacts.json", "w") or die("Unable to open file to write!");
-
     fwrite($myfile, $contacts);
     fclose($myfile);
-
-    $myfiler = fopen("contacts.json", "r") or die("Unable to open file to read!");
-    $obj = fread($myfiler,filesize("contacts.json"));
-    fclose($myfiler);
-
-    echo $obj;
+    echo "Contacts saved";
 ?>
